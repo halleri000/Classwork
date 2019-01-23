ca8dc02becd530ad8544e61ac1bd09de2d13ef44
unknown <halleri000@warren.k12.in.us>
2019-01-23 13:42:39 -0500

    v

--- Variables/lesson2.js
+++ Variables/lesson2.js
@@ -2,15 +2,19 @@
 
 $(documnent).ready(function(){
 
-    $("mathWrong").click(function(){
-        wrong();
+    $("#add").click(function(){
+        add();
     });
-
+    $("#divide").click(function(){
+        divide();
+    });
+    $("#modulo").click(function(){
+        modulus();
 });
 
 
 
-var wrong = function(){
+/*var wrong = function(){
 
     var x = $("#number 1").val();
     var y = $("#number 2").val();
@@ -19,12 +23,12 @@ var wrong = function(){
     var message = x + " + " + y + " = " + sum;
 
     $("#output").text(message + "  All information from the user is considered STRING datatype. The '+' sign, means to put the two strings together.");
-}
+}*/
 
-var right = function(){
+var add = function(){
 
-    var x = $("#number 1").val();
-    var y = $("#number 2").val();
+    var x = $("#number1").val();
+    var y = $("#number2").val();
     
     x = parseInt(x);
     y = parseInt(y);
@@ -34,4 +38,29 @@ var right = function(){
 
     $("#output").text(message + "  All information from the user is considered STRING datatype. The '+' sign, means to put the two strings together.");
 
+}
+
+var divide = function(){
+
+    var x = $("#number1").val();
+    var y = $("#number2").val();
+    
+    /*x = parseInt(x);
+    y = parseInt(y);*/
+
+    var quotient = x / y;
+    $('#output').text(quotient);
+}
+
+var modulus = function(){
+
+    var x = $("#number1").val();
+    var y = $("#number2").val();
+
+    var modulus = x % y;
+    $('#output').text(modulus);
+}
+var time = function(){
+    //xwill be converting from Days to hours/mins/seconds
+    //y will be converting from seconds to days
 }
