a0fd7c0d03c739ae8da87db6cbcf982dd6225809
halleric <42150930+halleric@users.noreply.github.com>
2019-02-05 10:56:45 -0500

    Update lesson3.js

--- functions/lesson3.js
+++ functions/lesson3.js
@@ -1,8 +1,6 @@
-
 $("#document").ready(function(){
 
     $("#button").click(function(){
-
         calculateMidpoint();
     
     })
@@ -12,12 +10,12 @@ $("#document").ready(function(){
 
 var calculateMidpoint = function(){
 
-    var x1 = parseInt($("#inbput1").val());
-    var x2 = parseInt($("#inbput2").val());
-    var y1 = parseInt($("#inbput3").val());
-    var y2 = parseInt($("#inbput4").val());
-    var x3 = parseInt($("#inbput5").val());
-    var y3 = parseInt($("#inbput6").val());
+    var x1 = parseInt($("#input1").val());
+    var x2 = parseInt($("#input3").val());
+    var y1 = parseInt($("#input2").val());
+    var y2 = parseInt($("#input4").val());
+    var x3 = parseInt($("#input5").val());
+    var y3 = parseInt($("#input6").val());
 
     midpoint(x1, y1, x2, y2);
     midpoint(x2, y2, x3, y3);
@@ -30,5 +28,5 @@ var midpoint = function( x1, y1, x2, y2 ){
     var midpointX = (x1 + x2)/2;
     var midpointY = (y1 + y2)/2;
 
-$("output").append("The midpoint is: (" + midpointX + "," + midpointY + ")");
+$("#output").append("The midpoint is: (" + midpointX + "," + midpointY + ")");
 }
