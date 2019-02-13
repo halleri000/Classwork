4488ec5fecf3a7549e26d7c0d4aa334c1d1b6413
unknown <halleri000@warren.k12.in.us>
2019-02-13 13:43:25 -0500

    bg

--- west_RPG/wildwest.js
+++ west_RPG/wildwest.js
@@ -4,46 +4,50 @@ var hasPhaser = false;
 
 $(document).ready(function(){
 
-    
-    
-    var button1 = createButton('option3', O3);
-    var button2 = createButton('option4', O4);
-    var button3 = createButton('option5', O5);
-    var button4 = createButton('option6', O6);
-    var button5 = createButton('option7', O7);
-    var button6 = createButton('option8', O81);
-    var button7 = createButton('option8', O82);
+
     
     updateNarrative(N1);
-    $('footer').html(createButton("option1", O1) + createButton("option2", O2) );
+    $('footer').html(createButton("button1", O1) + createButton("button2", O2) + createButton("button3", O9) );
+    $("button3").hide();
 
-    $("#option1").click(function(){
-        updateNarrative(N2);
-        $('footer').html(button1 + button2 + button3);
-        hasBow = true;
-    })
+    $("#button1").click(function(){ 
 
-    $("#option2").click(function(){
-        updateNarrative(N3)
-        $('footer').html(createButton(button4 + button5));
-        hasPhaser = true;
-    })
-
-
-    $("#option3").click(function(){
-        var roleButton;
-        if(hasBow){
-            roleButton = createButton(button6);
+        if($('#button1').text() === O1){
+            updateNarrative(N2);
+            $('#button1').text(O3)
+            $('#button2').text(O4)
+            $('#button3').text(O5)
+            $('#button3').show()
         }
-        else
-            roleButton = createButton(button7);
-       //updateNarrative(N5);
-        //$('footer').html(roleButton);
+        else if($('#button1').text() === O3){
+        updateNarrative(N5);
+        $('#button1').text(O81);
+        $('#button2').hide();
+        }
     })
 
-    $("#option6").click(function(){
-        updateNarrative(N6);
-        $('footer').html(roleButton);
+    $("#button2").click(function(){ 
+        if($('#button2').text() === O2){
+        updateNarrative(N3);
+        $('#button1').text(O6);
+        $('#button2').text(O7);
+        }
+        else if($('#button2').text() ===O7){
+            updateNarrative(N6);
+            $('#button2').text(O82);
+            $('#button1').hide();
+        }
+        
+    })
+
+    $("#button3").click(function(){
+        if($('#button3').text() === O9){
+            updateNarrative(N7);
+            $('#button1').text(O10);
+            $('#button2').text(O11); 
+            $('#button3').hide();
+            }
+    
     })
 
 
@@ -55,6 +59,8 @@ var N2 = "This ranger was the most dangerous archer known to man. He loved to ..
 var N3 = "This robot had killer instincts. Purge the wild west of all evil-doers. Armed with a phaser he..."
 var N5 = "The ranger approaches the insidious Mr. Pig. He raises his bow and ... "
 var N6 = "The robot approaches the insidious Mr. Pig. He raises his phaser and ..."
+var N7 = "The troll began to get prepared to help Mr. Pig by..."
+var N8 = " The troll started making people..."
 var O1 = "ranger."
 var O2 = "robot."
 var O3 = "chase tumbleweeds."
@@ -64,6 +70,11 @@ var O6 = "heats his pork and beans."
 var O7 = "purges the wild west of all evil-doers."
 var O81 = "shoots the arrow through Mr.Pig."
 var O82 = "fires, vaporizing Mr. Pig."
+var O9 = "tall troll"
+var O10 = "Shooting the ranger..."
+var O11 = " Go to bed"
+
+
 
 var updateNarrative = function(a){
 
