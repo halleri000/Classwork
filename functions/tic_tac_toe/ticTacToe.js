eddf2e966cd6c537b2cc6287c0b96a7753dbd9bf
unknown <halleri000@warren.k12.in.us>
2019-03-08 13:44:15 -0500

    vf

--- functions/tic_tac_toe/ticTacToe.js
+++ functions/tic_tac_toe/ticTacToe.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
 
+var turn = 1;
+
+$('td').click(function(){
+
+    if($(this).text() === ''){
+        if(turn%2 === 1){
+            $(this).text("X");
+        }else 
+            $(this).text("O");
+            turn++;
+    }
+
+})
+
+$('button').click(function(){
+
+    turn = 1;
+
+})
+
+
 });
+
+var checkWin = function(){
+
+}
+
+var reset = function(){
+
+}
