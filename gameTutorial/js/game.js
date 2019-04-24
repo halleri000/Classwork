3516d1429cff24d1a6653943559bb7ae675639c2
unknown <halleri000@warren.k12.in.us>
2019-04-24 13:37:20 -0400

    h

--- gameTutorial/js/game.js
+++ gameTutorial/js/game.js
@@ -40,12 +40,79 @@ gameScene.create = function(){
   // goal
   this.treasure = this.add.sprite(this.sys.game.config.width - 80, this.sys.game.config.height / 2, 'treasure');
   this.treasure.setScale(0.6);
+
+    // group of enemies
+    this.enemies = this.add.group({
+      key: 'dragon',
+      repeat: 5,
+      setXY: {
+        x: 110,
+        y: 100,
+        stepX: 80,
+        stepY: 20
+      }
+    });
+  Phaser.Actions.ScaleXY(this.enemies.getChildren(), -0.5, -0.5);
+  Phaser.Actions.Call(this.enemies.getChildren(), function(enemy) {
+    enemy.speed = Math.random() * 2 + 1;
+  }, this);
+  this.isPlayerAlive = true;
+
+  this.cameras.main.resetFX();
 }
 
 gameScene.update = function(){
+  if(!this.isPlayerAlive){
+    return;
+  }
 
     if(this.input.activePointer.isDown){
         //player walks
         this.player.x += this.playerSpeed;
     }
-}
+
+    //treasure collision
+    if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), this.treasure.getBounds())) {
+      this.gameOver();
+    }}
+ // enemy movement and collision
+ let enemies = this.enemies.getChildren();
+ let numEnemies = enemies.length;
+
+ for (let i = 0; i < numEnemies; i++) {
+
+   // move enemies
+   enemies[i].y += enemies[i].speed;
+
+   // reverse movement if reached the edges
+   if (enemies[i].y >= this.enemyMaxY && enemies[i].speed > 0) {
+     enemies[i].speed *= -1;
+   } else if (enemies[i].y <= this.enemyMinY && enemies[i].speed < 0) {
+     enemies[i].speed *= -1;
+   }
+
+   // enemy collision
+   if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), enemies[i].getBounds())) {
+     this.gameOver();
+     break;
+   }
+ }
+// end the game
+
+gameScene.gameOver = function() {
+ 
+  // flag to set player is dead
+  this.isPlayerAlive = false;
+ 
+  // shake the camera
+  this.cameras.main.shake(500);
+
+  this.time.delayedCall(250, function(){
+    this.cameras.main.fade(250);
+  }, [], this);
+ 
+  // restart game
+  this.time.delayedCall(500, function() {
+    this.scene.restart();
+  }, [], this);
+};
